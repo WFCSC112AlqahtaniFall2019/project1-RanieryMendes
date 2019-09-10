84342762cc5be6642ad54242d89127bc704e1fb7
ranierymendes <mendrc18@wfu.edu>
2019-09-09 22:41:26 -0400

    Commit #4

--- Battleship.cpp
+++ Battleship.cpp
@@ -7,27 +7,25 @@ using namespace std;
 
 int main() {
 
-    //variables to get user's inputs
+    //variables to get the user's inputs
     int rowUser;
     int columnUser;
 
+    //declare and initialize variables that will set the vectors' lengths
     int row = 3;
     int column = 3;
 
-    vector <int> vecRow (row,0);
-    vector < vector <int> > matrix (column,  vecRow) ;
+    // matrix
+    vector <string> board (3);
+    vector <vector <string> > matrixTwo (3, board);
+
     srand(time(0));
 
-
-    cout << "Hello! " << endl << "Please enter the position of the row and column you would like to mark on the board. They are from 1 to 3 " << endl;
-
-
-
-     vector <string> board (3);
-     vector <vector <string> > matrixTwo (3, board);
+    //Instructions to the user
+    cout << "Hello! Welcome to Battlefield! " << "Let's play!" << endl << "Please enter the position of the row and column you would like to mark on the board. They are both from 1 to 3. Enter each one at a time. " << endl << endl;
 
 
-     // initializing the vectors within the board
+     // initializing the vectors within the matrix
 
      for (int i =0; i < 3; ++i){
          for (int j= 0; j < 3; ++j){
@@ -43,7 +41,7 @@ int main() {
     cout << " " << matrixTwo.at(2).at(0) << " " << "| " << matrixTwo.at(2).at(1) << " | " << matrixTwo.at(2).at(2) << endl;
     cout << " " << endl;
 
-// setting a random location for the ship
+// set a random location for the ship
     int rowComputer;
     int columnComputer;
     rowComputer = rand() % 2;
@@ -52,8 +50,9 @@ int main() {
 
     int guesses = 1; // counter for the number of guesses made by the user
 
-    // loop for interactions with the user
+    // loop for  the interaction with the user
     bool  game = true;
+
      while (game) {
 
 
@@ -68,7 +67,7 @@ int main() {
          if ((rowUser == rowComputer) && (columnUser == columnComputer))
          {
             matrixTwo.at(rowUser).at(columnComputer) = "O";
-            cout << "Congratulations!! You won the game. The ship was at row " << rowUser + 1 << " column "<< columnUser + 1 << ". You sunk the ship in " << guesses << " guesses." << endl << " "<< endl;
+            cout << "Congratulations!! You won the game. The ship was at row " << rowUser + 1 << " column "<< columnUser + 1 << ". You sunk it in " << guesses << " guesses." << endl << " "<< endl;
              cout << " " << matrixTwo.at(0).at(0) << " " << "| " << matrixTwo.at(0).at(1) << " | " << matrixTwo.at(0).at(2) << endl;
              cout << "-----------" << endl;
              cout << " " << matrixTwo.at(1).at(0) << " " << "| " << matrixTwo.at(1).at(1) << " | " << matrixTwo.at(1).at(2) << endl;
@@ -79,6 +78,8 @@ int main() {
 
           game = false;
          }
+
+
         else {
              cout << " " << matrixTwo.at(0).at(0) << " " << "| " << matrixTwo.at(0).at(1) << " | "
                   << matrixTwo.at(0).at(2) << endl;
@@ -90,6 +91,7 @@ int main() {
                   << matrixTwo.at(2).at(2) << endl;
              cout << " " << endl;
 
+// re-initialize the vectors within the matrix
 
              for (int i = 0; i < 3; ++i) {
                  for (int j = 0; j < 3; ++j) {
@@ -101,22 +103,9 @@ int main() {
              cout <<  "Please try again. Enter the position of the row and column you would like to mark on the board. They are from 1 to 3 " << endl << " " << endl;
 
          }
-         guesses++;
+
+         guesses++; // increment the number of guesses
      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     return 0;
 }
